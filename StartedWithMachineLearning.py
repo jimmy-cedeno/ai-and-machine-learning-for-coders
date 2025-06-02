fbe068c1459872da23ebfdf3b662408994fdb001
Jimmy Cedeno <jcedeno@peigo.com.ec>
2025-06-02 09:59:53 -0500

    Add Learned log

--- StartedWithMachineLearning.py
+++ StartedWithMachineLearning.py
@@ -2,12 +2,14 @@ import numpy as np
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
 
-model = Sequential([Dense(units=1, input_shape=[1])])
-model.compile(optimizer='sgd', loss='mean_squared_error')
+dense = Dense(units=1, input_shape=[1])
+model = Sequential([dense])
+model.compile(optimizer='sgd', loss='mean_squared_error')  # sgd => stochastic gradient descent
 
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
 ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
 
 model.fit(xs, ys, epochs=500)
 
+print("Learned: {}".format(dense.get_weights()))
 print(model.predict(np.array([10.0])))
