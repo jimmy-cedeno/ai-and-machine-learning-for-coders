ec84e809ac2650925cd1731023cba59c1b380dc3
Jimmy Cedeno <jcedeno@peigo.com.ec>
2025-08-16 16:28:16 -0500

    Chapter 7 lstm and glove

--- chapter7/sarcasm_glove.py
+++ chapter7/sarcasm_glove.py
@@ -1,14 +1,12 @@
 import json
 import tensorflow as tf
+import numpy as np
 
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from bs4 import BeautifulSoup
 import string
-import matplotlib.pyplot as plt
-
-from collections import OrderedDict
-import numpy as np
+import matplotlib.pyplot as plt 
 import io
 
 stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at",
@@ -26,6 +24,17 @@ stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "a
              "yourselves"]
 
 table = str.maketrans('', '', string.punctuation)
+
+glove_embeddings = dict()
+f = open('glove.twitter.27B.100d.txt')
+for line in f:
+  values = line.split()
+  word = values[0]
+  coefs = np.asarray(values[1:], dtype='float32')
+  glove_embeddings[word] = coefs
+
+f.close()
+print('Loaded %s word vectors.' % len(glove_embeddings))
 
 with open('sarcasm.json', 'r') as f:
   datastore = json.load(f)
@@ -51,29 +60,13 @@ for item in datastore:
   labels.append(item['is_sarcastic'])
   urls.append(item['article_link'])
 
-xs = []
-ys = []
-current_item = 1
-for item in sentences:
-  xs.append(current_item)
-  current_item += 1
-  ys.append(len(item))
-
-newys = sorted(ys)
-
-plt.plot(xs, newys)
-plt.axis([26000, 27000, 50, 250])
-plt.show()
-
-print(newys[26000])
-
-vocab_size = 2000
-embedding_dim = 7
-max_length = 100
+vocab_size = 13200
+embedding_dim = 100
+max_length = 50
 trunc_type = 'post'
 padding_type = 'post'
 oov_tok = ''
-training_size = 24000
+training_size = 23000
 
 training_sentences = sentences[:training_size]
 testing_sentences = sentences[training_size:]
@@ -84,61 +77,76 @@ tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
 tokenizer.fit_on_texts(training_sentences)
 
 word_index = tokenizer.word_index
-
+print(len(word_index))
 training_sequences = tokenizer.texts_to_sequences(training_sentences)
-training_padded = pad_sequences(training_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+training_paddded = pad_sequences(training_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
 testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
 testing_padded = pad_sequences(testing_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
 
-wc = tokenizer.word_counts
-print(wc)
+xs=[]
+ys=[]
+cumulative_x=[]
+cumulative_y=[]
+total_y=0
+for word, index in tokenizer.word_index.items():
+  xs.append(index)
+  cumulative_x.append(index)
+  if glove_embeddings.get(word) is not None:
+    total_y = total_y + 1
+    ys.append(1)
+  else:
+    ys.append(0)
+  cumulative_y.append(total_y / index)
 
-newlist = (OrderedDict(sorted(wc.items(), key=lambda t: t[1], reverse=True)))
-print(word_index)
-print(newlist)
+fig, ax = plt.subplots(figsize=(12,2))
+ax.spines['top'].set_visible(False)
 
-xs = []
-ys = []
-curr_x = 1
-for item in newlist:
-  xs.append(curr_x)
-  curr_x += 1
-  ys.append(newlist[item])
+plt.margins(x=0, y=None, tight=True)
+# plt.axis([13000, 14000, 0, 1])
+plt.fill(ys)
 
-print(ys)
-plt.plot(xs, ys)
-plt.axis([300, 10000, 0, 100])
-plt.show()
-print(ys[1000])
-print(ys[2000])
+plt.plot(cumulative_x, cumulative_y)
+plt.axis([13000, 14000, .97, .98])
+print(cumulative_y[-1:])
 
-print(ys[3125])
-print(ys[10000])
-print(ys[12156])
+embedding_matrix = np.zeros((vocab_size, embedding_dim))
+for word, index in tokenizer.word_index.items():
+  if index > vocab_size - 1:
+    break
+  else:
+    embedding_vector = glove_embeddings.get(word)
+    if embedding_vector is not None:
+      embedding_matrix[index] = embedding_vector
 
-training_padded = np.array(training_padded)
+print(embedding_matrix[2])
+print(tokenizer.word_index)
+
+training_paddded = np.array(training_paddded)
 training_labels = np.array(training_labels)
 testing_padded = np.array(testing_padded)
 testing_labels = np.array(testing_labels)
 
 model = tf.keras.Sequential([
-  tf.keras.layers.Embedding(vocab_size, embedding_dim),
-  tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(100)),
-  tf.keras.layers.Dense(32, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.1)),
+  tf.keras.layers.Embedding(vocab_size, embedding_dim, weights=[embedding_matrix], trainable=False),
+  tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(embedding_dim, return_sequences=True)),
+  tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(embedding_dim)),
+  tf.keras.layers.Dense(24, activation='relu'),
   tf.keras.layers.Dense(1, activation='sigmoid')
 ])
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+adam = tf.keras.optimizers.Adam(learning_rate=0.00001, beta_1=0.9, beta_2=0.999, amsgrad=False)
+model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
 model.summary()
 
-num_epochs = 150
-history = model.fit(training_padded, training_labels, epochs=num_epochs, validation_data=(testing_padded, testing_labels), verbose=2)
+num_epochs = 30
+history = model.fit(training_paddded, training_labels, epochs=num_epochs, validation_data=(testing_padded, testing_labels), verbose=2)
 
 def plot_graphs(history, string):
   plt.plot(history.history[string])
-  plt.plot(history.history['val_' + string])
+  plt.plot(history.history['val_'+string])
   plt.xlabel('Epochs')
   plt.ylabel(string)
-  plt.legend([string, 'val_' + string])
+  plt.legend([string, 'val_'+string])
   plt.show()
 
 plot_graphs(history, 'accuracy')
@@ -149,7 +157,7 @@ reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 def decode_sentence(text):
   return ' '.join([reverse_word_index.get(i, '?') for i in text])
 
-print(decode_sentence(training_padded[2]))
+print(decode_sentence(training_paddded[2]))
 print(training_sentences[2])
 print(labels[2])
 
@@ -162,7 +170,6 @@ print(weights[2])
 
 out_v = io.open('vecs.tsv', 'w', encoding='utf-8')
 out_m = io.open('meta.tsv', 'w', encoding='utf-8')
-
 for word_num in range(1, vocab_size):
   word = reverse_word_index[word_num]
   embeddings = weights[word_num]
@@ -175,7 +182,8 @@ test_sentences = ["It Was, For, Uh, Medical Reasons, Says Doctor To Boris Johnso
                   "It's a beautiful sunny day",
                   "I lived in Ireland, so in High School they made me learn to speak and write in Gaelic",
                   "Census Foot Soldiers Swarm Neighborhoods, Kick Down Doors To Tally Household Sizes"]
-
 sequences = tokenizer.texts_to_sequences(test_sentences)
 padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
 predictions = model.predict(padded)
+print(predictions)
