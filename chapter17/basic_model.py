290f3a41ad2c55ff73a200f54655edebaabc9e6c
Jimmy Cedeno <jcedeno@peigo.com.ec>
2025-09-24 05:36:26 -0500

    Chapter 17

---
+++ chapter17/basic_model.py
@@ -0,0 +1,19 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+
+l0 = Dense(units=1, input_shape=[1])
+model = Sequential([l0])
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
+
+model.fit(xs, ys, epochs=500, verbose=0)
+
+print(model.predict(np.array([[10.0]])))
+print("Here is what I learned: {}".format(l0.get_weights()))
+
+tf.saved_model.save(model, './saved_model/')
+# model.save('saved_model.h5')
